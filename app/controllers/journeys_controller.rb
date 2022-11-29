563c02d479064f57b44abd3d616a120f36b629b7
Anoula Croville <anoula.croville@yahoo.fr>
2022-11-29 16:16:39 +0100

    new creation controllers

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -5,14 +5,21 @@ class JourneysController < ApplicationController
   end
 
   def new
-    
+    @journey = Journey.new
   end
 
   def create
-
+    @journey = Journey.new(journey_params)
+    if @journey.save!
+      redirect_to journey(@journey)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def update
+  private
 
+  def journey_params
+    params.require(:journey).permit(:name, :budget_total)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :journeys, through: :journey_members
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
 end
