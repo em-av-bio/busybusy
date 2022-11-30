248da5a9cb46201ae58275ac8ea04bdbefda432a
gaelle ozanon <ozanongaelle@live.fr>
2022-11-30 17:12:35 +0100

    init_sortable

--- app/javascript/plugins/init_sortable.js
+++ app/javascript/plugins/init_sortable.js
@@ -1,18 +1,22 @@
 import Sortable from "sortablejs"
 
 const list = document.querySelector(".cards-sortable")
-const listItems = document.querySelectorAll(".cards")
+const listItems = document.querySelectorAll(".cards-sortable .card")
 
 const initSortable = () => {
   Sortable.create(list, {
+    dataIdAttr: 'data-ranking',
     ghostClass: "ghost",
     swap: true,
     animation: 150,
     onSort: (event) => {
       console.log(event.oldIndex + ' -> ' + event.newIndex);
+      listItems.forEach((item) => {
+        item.dataset.ranking = event.newIndex;
+        console.log(item.dataset.ranking);
+      });
     }
-    }
-  )
+  });
 }
 
 export { initSortable }
