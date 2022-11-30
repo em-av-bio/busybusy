bf5c0d6a5ed67a8c88683668b6f8ccd3866fc38f
kevin thomas <kevindemorzine@gmail.com>
2022-11-30 17:26:14 +0100

    scraping

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-
+require "nokogiri"
+require "net/http"
+require "open-uri"
+JourneyMember.destroy_all
+Journey.destroy_all
+User.destroy_all
 u1 = User.create!(nickname: "Gaëlle", email: "gaelle@toto.com", password: "azerty")
 u2 = User.create!(nickname: "Anoula", email: "anoula@toto.com", password: "azerty")
 u3 = User.create!(nickname: "Emmanuel", email: "emmanuel@toto.com", password: "azerty")
 u4 = User.create!(nickname: "JeanRo", email: "jeanro@toto.com", password: "azerty")
+
+def getRandImage(city)
+  url = "https://www.bing.com/images/search?q=#{city}&qft=+filterui:imagesize-wallpaper+filterui:photo-photo&form=IRFLTR&first=1&tsc=ImageHoverTitle"
+  html = URI.open(url)
+  doc = Nokogiri::HTML(open(url))
+  puts doc
+  img = doc.css("img").select { |link| link["src"].nil? == false && link["src"].include?("https://") }.map { |link| link["src"] }.sample
+  img.gsub!("w=", "")
+  img.gsub!("h=", "")
+  p img
+end
+
+city = gets.chomp
+
+getRandImage(city)
