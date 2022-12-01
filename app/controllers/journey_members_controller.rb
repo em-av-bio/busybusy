755a21ada8bd1066d1615b34346b3cfb988a74b8
gaelle ozanon <ozanongaelle@live.fr>
2022-12-01 17:57:44 +0100

    JourneyMember status

--- app/controllers/journey_members_controller.rb
+++ app/controllers/journey_members_controller.rb
@@ -21,18 +21,21 @@ class JourneyMembersController < ApplicationController
 
   def edit
     @journey = Journey.find(params[:journey_id])
-    @journey_member = current_user
+    @journey_member = JourneyMember.find(params[:id])
   end
 
   def update
     @journey_member = JourneyMember.find(params[:id])
     @journey_member.update(journey_member_params)
-    redirect_to journey_journey_locations_path(@journey_member.journey)
+    @journey_member.budget_defined!
+    if @journey_member.save!
+      redirect_to journey_journey_locations_path(@journey_member.journey)
+    end
   end
 
   private
 
   def journey_member_params
-    params.require(:journey_member).permit(:user_id)
+    params.require(:journey_member).permit(:budget)
   end
 end
