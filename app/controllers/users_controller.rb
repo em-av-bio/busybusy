5b31a3c79eef2504c8ea3fe74a2d1e0ef3644f79
gaelle ozanon <ozanongaelle@live.fr>
2022-12-04 12:50:56 +0100

    search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.All
   end
+
+  def search
+    @users = User.where('nickname ILIKE ?', "%#{params[:nickname_search]}%").order(created_at: :desc)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.update('search_results',
+          partial: 'users/search_results',
+          locals: { users: @users })
+        ]
+      end
+    end
+  end
 end
