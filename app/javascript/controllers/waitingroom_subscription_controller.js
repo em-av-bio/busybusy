515a1d580b11327c1cc60a4a0ef7f66c183f86c2
gaelle ozanon <ozanongaelle@live.fr>
2022-12-06 19:39:22 +0100

    action cable enum

--- app/javascript/controllers/waitingroom_subscription_controller.js
+++ app/javascript/controllers/waitingroom_subscription_controller.js
@@ -3,8 +3,8 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="waitingroom-subscription"
 export default class extends Controller {
-  static values = { journeyId: Number, currentUserId: Number}
-  static targets = ["waitingUser"]
+  static values = { journeyId: Number, currentUserId: Number, currentUserStatus: Number}
+  static targets = ["waitingUser", "button"]
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
@@ -17,20 +17,29 @@ export default class extends Controller {
           console.log(`Subscribe to the waitingroom with the id ${this.journeyIdValue}.`);
         },
         received: data => {
+
           this.changeStatus(data.json)
+          this.checkIfAllGood(data.json)
         }
       }
     )
   }
 
   changeStatus(json) {
-    console.log(json);
-    const idSelector = `#status-${json.journeyMemberId}`
-    console.log(idSelector);
-    const journeyMemberStatus = document.querySelector(idSelector)
-    console.log(journeyMemberStatus);
-    journeyMemberStatus.innerText = "Validé"
-    journeyMemberStatus.classList.remove("warning")
-    journeyMemberStatus.classList.add("good")
+    console.log("My current Status", this.currentUserStatusValue);
+    console.log("Other member Status", json.status);
+    if (json.status >= this.currentUserStatusValue) {
+      const idSelector = `#status-${json.journeyMemberId}`
+      const journeyMemberStatus = document.querySelector(idSelector)
+      journeyMemberStatus.innerText = "Validé"
+      journeyMemberStatus.classList.remove("warning")
+      journeyMemberStatus.classList.add("good")
+    }
+  }
+
+  checkIfAllGood(json) {
+    if (json.status >= this.currentUserStatusValue && json.allGood) {
+      this.buttonTarget.innerText = "Passez à l'étape suivante !"
+    }
   }
 }
