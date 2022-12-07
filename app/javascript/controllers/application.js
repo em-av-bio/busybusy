c5929667e3a0c65d795f7e626492a62acb8d3d2d
gaelle ozanon <ozanongaelle@live.fr>
2022-12-07 11:49:14 +0100

    fix drag and drop

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -12,10 +12,12 @@ const clickBtn = document.getElementById("clickBtn");
 const popup = document.getElementById("popup");
 const closeBtn = document.getElementById("closeBtn");
 
+
+if (clickBtn) {
 clickBtn.addEventListener('click', ()=>{
     popup.style.display = 'block';
 });
 closeBtn.addEventListener('click', ()=>{
     popup.style.display = 'none';
 });
-
+}
